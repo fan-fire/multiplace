1419f714f583ca0fe521c46ed156724b9ab26f4b
louwjlab <louwjlabuschagne@gmail.com>
2022-05-23 21:41:15 +0200

    check CI/CD tests

---
+++ .github/scripts/testing.sh
@@ -0,0 +1,19 @@
+echo $(pwd)
+# cd ../../
+# echo $(pwd)
+printenv
+# if CI is set, then we are running on github need to install eveyrthing
+if [ "$CI" == "true" ]; then
+    echo "CI is set"
+    npm ci
+    nvm use v16.14.2
+else
+    echo "CI is not set"
+fi
+
+echo "Running npx hardhat coverage"
+npx hardhat coverage >>test_output.log
+sed -i '1s/^/```\n/' test_output.log
+echo '```' >>test_output.log
+echo "Test output"
+cat test_output.log
